74462364bec86f3d6f86c1e6e5038746c94bcc3a
= <=>
2019-09-25 16:33:14 -0700

    Added form to 'New Compaign' page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,13 +1,27 @@
 import React, { Component } from 'react';
 import CommonPage from '../../components/CommonPage';
+import { Form, Button, Input } from 'semantic-ui-react';
 
 class NewCampaign extends Component {
-  state = {}
+  state = {
+    minimumContribution: ''
+  }
+
   render() {
     return (
-    <CommonPage>
-      <h3>New Campaign!</h3>
-    </CommonPage>);
+      <CommonPage>
+        <h3>New Campaign!</h3>
+        <Form>
+          <Form.Field>
+            <label>Minimum Contribution</label>
+            <Input label='wei' labelPosition='right' type='number' value={this.state.minimumContribution} onChange={(event) => {
+              this.setState({ minimumContribution: event.target.value })
+            }} />
+          </Form.Field>
+          <Button primary>Create!</Button>
+        </Form>
+      </CommonPage>
+    );
   }
 }
 
