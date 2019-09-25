65b1d733d0c7c643d9f69368fed33e889480c6f2
= <=>
2019-09-25 15:13:12 -0700

    Added semantic-ui to the project

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,38 @@
 import React, { Component } from 'react'
 import factory from '../ethereum/factory';
+import { Card } from 'semantic-ui-react';
+// import 'semantic-ui-css/semantic.min.css';
 
 export default class CampaignIndex extends Component {
-  async componentDidMount() {
-    console.log(await factory.methods.getDeployedCampaigns().call());
+
+  static async getInitialProps() {
+    const campaigns = await factory.methods.getDeployedCampaigns().call();
+    // console.log(campaigns[0]);
+    return { campaigns };
+  }
+
+  renderCampaigns() {
+    const items = this.props.campaigns.map(address => {
+      return {
+        header: address,
+        description: <a>View Campaign</a>,
+        fluid: true
+      }
+    });
+
+    return <Card.Group items={items} />
   }
 
   render() {
     return (
       <div>
+        <link rel="stylesheet" href="//cdn.jsdelivr.net/npm/semantic-ui@2.4.2/dist/semantic.min.css" />
         This is the home page!
+        <hr/>
+        { this.renderCampaigns() }
+        <hr/>
+        Hello
       </div>
-    )
+    );
   }
 }
