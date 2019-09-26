5675d04ad7b4fbad718e11b2e29c300e471e4da6
= <=>
2019-09-25 20:47:23 -0700

    Added more functionality to 'New Campaign' page

--- pages/campaigns/new.js
+++ pages/campaigns/new.js
@@ -1,24 +1,53 @@
 import React, { Component } from 'react';
 import CommonPage from '../../components/CommonPage';
-import { Form, Button, Input } from 'semantic-ui-react';
+import factory from '../../ethereum/factory';
+import { Form, Button, Input, Message } from 'semantic-ui-react';
+import web3 from './../../ethereum/web3';
+import next from 'next';
 
 class NewCampaign extends Component {
   state = {
-    minimumContribution: ''
+    minimumContribution: '',
+    errorMessage: '',
+    loading: false
   }
+
+  
+  onSubmit = async (event) => {
+
+    try {
+      // Start the loading circle and reset the error message
+      this.setState({ loading: true, errorMessage: '' })
+
+      // Retrieve user accounts and create a new campaign using the CampaignFactory
+      const accounts = await web3.eth.getAccounts();
+      await factory.methods
+        .createCampaign(this.state.minimumContribution)
+        .send({
+          from: accounts[0]
+        });  
+      } catch (err) {
+        this.setState({ errorMessage: err.message });
+      }
+      this.setState({ loading: false });
+
+      
+  };
 
   render() {
     return (
       <CommonPage>
         <h3>New Campaign!</h3>
-        <Form>
+        <Form onSubmit={this.onSubmit} error={!!this.state.errorMessage}>
           <Form.Field>
             <label>Minimum Contribution</label>
             <Input label='wei' labelPosition='right' type='number' value={this.state.minimumContribution} onChange={(event) => {
               this.setState({ minimumContribution: event.target.value })
             }} />
           </Form.Field>
-          <Button primary>Create!</Button>
+
+          <Message error header='Something went wrong' content={this.state.errorMessage}/>
+          <Button primary loading={this.state.loading}>Create!</Button>
         </Form>
       </CommonPage>
     );
